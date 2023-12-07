70d8e540b11b974475be2fe3ae2667769ca20131
Depyter <acabalharleyvan@gmail.com>
2023-12-07 17:17:57 +0800

    bug fix on line 240 to 250

--- game.c
+++ game.c
@@ -19,6 +19,7 @@ If the total sum of the bets from the players exceeds the capital of the banker 
 #include <time.h>
 #include <string.h>
 
+// Create a structure whose that has the player data 
 struct player_data
 {
     int money;
@@ -27,6 +28,7 @@ struct player_data
     int bet;
 };
 
+// Initally asks the player if they want to play
 int dialogue() {
     char playerInput;
     printf("Press Y to start or Q to quit.\n");
@@ -46,29 +48,16 @@ int dialogue() {
     }
 }
 
+// Prints an asterisk line
 void printLine() {
     int i;
-    for(i = 20; i > 0; i--) {
+    for(i = 25; i > 0; i--) {
         printf("*");
     }
     printf("\n");
 }
 
-/*void print_Border(height,width) {
-    int i;
-
-    // Print the sides with text
-    for (int i = 0; i < height; i++) {
-        printf("* %-*s *\n", width, (i == height / 2)); // Center text in the middle row
-    }
-}*/
-
-/* void printMoney() {
-    printLine();
-    printBorder();
-    printLine();
-} */
-
+// Randomly generates two dice rolls
 int sum_of_two_dice() {
     int i, n = 2, sum = 0, dice = 0;
 
@@ -83,6 +72,7 @@ int sum_of_two_dice() {
     return sum;
 }
 
+// Function to get the guess of the player and set the guess value inside the struct to the given value
 void get_guesses(struct player_data *p) {
     int guessing = 1, guess;
 
@@ -101,6 +91,7 @@ void get_guesses(struct player_data *p) {
     p->guess = guess;
 }
 
+// Function to get the bet of the player and set the bet value inside the struct to the given value
 void get_bets(struct player_data *p) {
     int bet, betting = 1;
         do {
@@ -139,7 +130,6 @@ void player_betting(int *banker, int *money_array[]) {
 
 }
 
-
 /* Apparently you can't remove an element from an array in c.
 So you have to create a custom function to create a new array*/
 // Pass the array, size of the array, and the element you want to remove. 
@@ -150,10 +140,17 @@ void removeElement(struct player_data data_array[], int *size, int indexToRemove
     (*size)--;
 }
 
+// Set the current array to the original values using a duplicate array.
+void resetArray(struct player_data data_array[], struct player_data resetArray[], int array_size) {
+    for (int i = 0; i < array_size; i++) {
+        data_array[i] = resetArray[i];
+    }
+}
+
 int main() {
 
     // Initalize the money for each player
-    int guessed = 0, runGame = 0;
+    int guessed = 0, runGame = 0, betting = 1, bet_sum = 0;
     char playerInput;
 
     // Used to randomize the generated dice
@@ -165,40 +162,52 @@ int main() {
     struct player_data player_three;
     struct player_data banker;
     
+    // Initialize Money and player name
     banker.money = 100000;
-
     player_one.money = 20000;
-    strcpy(player_one.name, "Harley");
-    
+    strcpy(player_one.name, "Harley"); 
     player_two.money = 20000;
     strcpy(player_two.name, "Venice");
-    
     player_three.money = 20000;
     strcpy(player_three.name, "AJ");
 
+    // Pass the custom structures to a struct array and have one has a reference array
     struct player_data data_array[3] = {player_one, player_two, player_three};
-
+    struct player_data reset_array[3] = {player_one, player_two, player_three};
     int array_size = sizeof(data_array) / sizeof(data_array[0]);
 
     runGame = dialogue();
+    // Main loop where we run the game and call functions for guessing and betting
     while (runGame) {
+
+        // Used to randomize dice rolls for the rand() function
         srand ((unsigned)time(&t1)); 
+
+        // Take each guess for each player
         for (int i = 0; i < array_size; i++) {
-            if (data_array[i].money <= 100) {
-                printf("%s does not have enough money to play anymore.", data_array[i].name);
-                removeElement(data_array, &array_size, i);
-            } else {
-                get_guesses(&data_array[i]);
-            }
+            get_guesses(&data_array[i]);
         }
 
-        for (int i = 0; i < array_size; i++) {
-            if (!(data_array[i].money <= 100)) {
-                get_bets(&data_array[i]);
+        // Takes the bets of each player but checks player bet exceeds banker money.
+        do {
+            for (int i = 0; i < array_size; i++) {
+                if (!(data_array[i].money <= 100)) {
+                    get_bets(&data_array[i]);
+                    bet_sum += data_array[i].money;
+                }
             }
-        }
 
+            if(!(bet_sum > banker.money)) {
+                betting = 0;
+            }
+            else {
+                printf("Overall bet is exceeds the bankers money (%i), please bet again.\n", banker.money);
+            }
+        } while (betting);
+
+        printLine();
         int sum = sum_of_two_dice();
+        printLine();
 
         for (int i = 0; i < 3; i++) {
             if (data_array[i].guess == sum) {
@@ -219,29 +228,67 @@ int main() {
         if(!guessed) {
             printf("No one guessed it right.\n");
         }
+
         printf("Money of each player and the banker is:\n");
         printf("Banker:%i\n", banker.money);
         for (int i = 0; i < array_size; i++) {
             printf("%s's money is: %i.\n",data_array[i].name, data_array[i].money);
         }
         printLine();
-
-        printf("Should we continue?\nPress Y to continue.\nPress R to restart each players money.\nQ to quit.\n");
-        scanf(" %c", &playerInput);
-        switch (playerInput)
-        {     
-        case 'Y': case 'y':
-            continue;   
-        case 'R': case 'r':
-            for (int i = 0; i < 3; i++) {
-                data_array[i].money = 20000;
+        
+        int i = 0;
+        while (i < array_size) {
+            // Removes the player from the game if they have less than 100 money. 
+            if (data_array[i].money <= 100) {
+                printf("%s does not have enough money to play anymore.\n", data_array[i].name);
+                removeElement(data_array, &array_size, i);
+                i = 0;
+            } else {
+                i++;
             }
-            continue;
-        case 'Q': case 'q':
-            return 0;
 
-        default:
-            return 0;
+        }
+
+        if (array_size > 1) {
+            printf("Should we continue?\nPress Y to continue.\nPress R to restart.\nQ to quit.\n");
+            scanf(" %c", &playerInput);
+
+            switch (playerInput)
+            {     
+            case 'Y': case 'y':
+                continue;   
+            case 'R': case 'r':
+                resetArray(data_array, reset_array, 3);
+                banker.money = 100000;
+                printf("Reseted the money");
+                continue;
+                
+            case 'Q': case 'q':
+                return 0;
+
+            default:
+                return 0;
+            } 
+        } 
+        // If one player is left, then ask the player if they want to quit or reset the game
+        else {
+            printf("There is only one player left.\nPress R to restart the game.\nQ to quit.\n");
+            
+            scanf(" %c", &playerInput);
+            switch (playerInput)
+            {     
+            case 'R': case 'r':
+                resetArray(data_array, reset_array, 3);
+                banker.money = 100000;
+                printf("The money is reset.");
+                continue;
+                
+            case 'Q': case 'q':
+                return 0;
+
+            default:
+                return 0;
+            }
         }
     }
 
